08cbd4e59b1ade28a02b50ef234b2e98d0455fca
takanori-terasako <pmbmbfb71mx4d@outlook.jp>
2022-09-10 11:01:34 +0900

    ハンバーガーメニュー完了

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-alert($("h1").text());
+
+/*
+hamberger_menu
+*/
+$(function() {
+  var headerHbBtn = $('.header-hb-area');
+  headerHbBtn.hide();
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 100) {
+      headerHbBtn.fadeIn();
+    } else {
+      headerHbBtn.fadeOut();
+    }
+  });
+});
+
+$(function(){
+  $('.header-hb-area').click(function(){
+    $('.header-hb-area, .header-nav').toggleClass('active');
+  });
+});
