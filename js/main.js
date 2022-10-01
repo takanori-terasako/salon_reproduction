802f74da986052d84114ade6708f27d2d9874f92
takanori-terasako <pmbmbfb71mx4d@outlook.jp>
2022-10-01 11:03:58 +0900

    スマポ画面完成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 /*
-hamberger_menu
+hamburger_menu
 */
 $(function() {
   var headerHbBtn = $('.header-hb-area');
@@ -17,5 +16,24 @@ $(function() {
 $(function(){
   $('.header-hb-area').click(function(){
     $('.header-hb-area, .header-nav').toggleClass('active');
+  });
+});
+
+/*
+scroll_fadeUp
+*/
+$(".js-fadeUp").on("inview", function () {
+  $(this).addClass("is-inview");
+});
+
+/*
+slick
+*/
+$(function () {
+  $(".description-slider").slick({
+    arrows: true,
+    dots: true,
+    prevArrow: '<img src="前へ矢印画像のパス" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="次へ矢印画像のパス" class="slide-arrow next-arrow">',
   });
 });
